98ebd42c06ea5996c082bf785d54f17c387c3f90
parazitol-official <73265222+parazitol-official@users.noreply.github.com>
2020-10-25 00:40:16 +0300

    1

---
+++ js/ch2.js
@@ -0,0 +1,19 @@
+(function() {
+    'use strict';
+
+    window.countryList = {
+         ch: {
+            name: "Schweiz",
+            code: "ch",
+            price: 69,
+            oldPrice: 138,
+            labelPrice: "CHF",
+            phoneHelper: "zum Beispiel, +41 123456789",
+            nameHelper: "Friedrich Muller",
+            nameError: "Der name erforderlich",
+            phoneError: "Numero incorrectement specifie",
+            countryError: "Nummer falsch angegeben",
+		isDefault: true
+        }
+    };
+})();
---
+++ js/translater.js
@@ -0,0 +1,254 @@
+var new_lang = {
+
+    "lt1": "EIN HARTER SCHLAG GEGEN DIE PARASITEN",
+
+    "lt2": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: visible; animation-iteration-count: 5; animation-name: pulse;\"></i> Normalisiert die Funktion des Magendarmtraktes",
+    
+    "lt3": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: visible; animation-iteration-count: 5; animation-name: pulse;\"></i> Vernichtet sämtliche Arten von Helminthen",
+    
+    "lt4": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: visible; animation-iteration-count: 5; animation-name: pulse;\"></i> Leitet aus dem Körper die Abfallprodukte der Parasiten heraus. ",
+    
+    "lt5": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: visible; animation-iteration-count: 5; animation-name: pulse;\"></i> Beugt einen Wiederbefall vor",
+    
+    "lt6": "EIN SONDERANGEBOT<br>",
+    
+    "lt7": "bis zum Ende der Aktion verbleiben",
+    
+    "lt8": "<div class=\"price_old\"></div>  Normalpreis",
+    
+    "lt9": "<div class=\"price_main wow pulse\" data-wow-delay=\"2s\" data-wow-iteration=\"3\" style=\"visibility: visible; animation-delay: 2s; animation-iteration-count: 3; animation-name: pulse;\"></div>  Aktionspreis",
+    
+    "lt10": "Bestellen",
+    
+    "lt11": "GEMÄSS DEN LETZTEN WHO-DATEN, <br>SIND ÜBER 1 MILLIARDE MENSCHEN VON PARASITEN BEFALLEN",
+    
+    "lt12": "90% von diesen ahnen nicht einmal, dass sie zu einem Nährstoffmileau für Helminthen wurden",
+    
+    "lt13": "SIND SIE SICHER, DASS SIE NICHT UNTER DIESEN SIND? ÜBERPRÜFEN SIE SICH AUF INFEKTIONSZEICHEN!",
+    
+    "lt14": "ANZEICHEN EINER HELMINTHOSE",
+    
+    "lt15": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Müdigkeitsgefühl",
+    
+    "lt16": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Kopfschmerzen, Unwohlsein",
+    
+    "lt17": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Entzündungen der oberen Atemwege",
+    
+    "lt18": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Reizbarkeit, Aggression",
+    
+    "lt19": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Alle Arten von Hautausschlägen",
+    
+    "lt20": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Gewichtsverlust, auch bei gutem Appetit ",
+    
+    "lt21": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Probleme mit dem Schlaf",
+    
+    "lt22": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Allergie (tränende Augen, Hautausschlag, Schnupfen, ",
+    
+    "lt23": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Häufige Erkältungen, Angina, Nasenverstopfung",
+    
+    "lt24": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Schmerzen in Gelenken und Muskeln",
+    
+    "lt25": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Vergrößerung der Lymphknoten",
+    
+    "lt26": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Verdauungsstörungen: Blähungen, Verstopfung oder Durchfall ",
+    
+    "lt27": "WEGE DER ANSTECKUNG MIT HELMINTHEN",
+    
+    "lt28": "Schlecht gewaschenes Obst, Gemüse und Beeren",
+    
+    "lt29": " Schlecht gebratenes Fleisch und Fisch",
+    
+    "lt30": "Kontaminiertes Wasser oder Boden",
+    
+    "lt31": "Kontakte mit den Haustieren",
+    
+    "lt32": "Öffentliche Plätze und Verkehrsmittel",
+    
+    "lt33": "Insektenstiche",
+    
+    "lt34": "Restaurants und Fastfood",
+    
+    "lt35": "FANGEN SIE MIT DER BEHANDLUNG BEREITS JETZT AN!",
+    
+    "lt36": "BEGINNEN",
+    
+    "lt37": "Parasiten, <br>die schreckliche Komplikationen auslösen:",
+    
+    "lt38": "ZYSTIZERKUS",
+    
+    "lt39": "Am häufigsten (über 60%) gelangt der Zystizerkus in das Gehirn, seltener in Skelettmuskulatur und die Augen. Er lebt zwischen 5 und 30 Jahren, löst <span>Tumore aus</span> ",
+    
+    "lt40": "SPRINGWÜRMER",
+    
+    "lt41": "Hausen und vermehren sich in den Verdauungsorganen und führen zur Bildung von <span>язв</span> и <span> umfangreicher Blutung </span> ",
+    
+    "lt42": "ANKYLOSTOMA",
+    
+    "lt43": "Beschädigen die Schleimhaut und ernähren sich von dem dabei freigesetzt werdenden Blut. Sie hinterlassen blutende Wunden, was zur Entwicklung einer<span> schweren Anämie</span> und <span>Sepsis führt</span> ",
+    
+    "lt44": "ALVEOKOKKEN",
+    
+    "lt45": "Es geschieht ein Absterben des Gewebes des betroffenen Organs. Außerdem werden die Blutgefäße zerstört und <span>hören auf, zu funktionieren</span> ",
+    
+    "lt46": "MULTIAKTIVE WIRKUNG <span class=\"fadeIn wow\" style=\"visibility: hidden; animation-name: none;\">Parazitol</span> ",
+    
+    "lt47": "VERNICHTET",
+    
+    "lt48": "alle möglichen Arten von Invasionen in kürzester Zeit",
+    
+    "lt49": " LEITET HERAUS",
+    
+    "lt50": "Leitet aus dem Körper die Abfallprodukte der Parasiten heraus",
+    
+    "lt51": "TRÄGT BEI ZUR",
+    
+    "lt52": "Reinigung des Organismus von Toxinen",
+    
+    "lt53": "STELLT WIEDER HER",
+    
+    "lt54": "Funktion des Immunsystems, endokrinen Systems, Verdauungs-, Nerven- und Herzkreislaufsystems",
+    
+    "lt55": "SCHÜTZT",
+    
+    "lt56": "vor erneuter Ansteckung, bildet ein pathogenes Mileau im Organismus des Menschen und verstärkt die Abwehrkraft",
+    
+    "lt57": "BESTELLEN",
+    
+    "lt58": "STARKE NATÜRLICHE REZEPTUR <span class=\"fadeIn wow\" style=\"visibility: hidden; animation-name: none;\">Parazitol</span> ",
+    
+    "lt59": "MEERRETTICHBAUM",
+    
+    "lt60": "Aufgrund seiner einzigartigen Rezeptur bereinigt er den Organismus von Schadstoffen und schweren Metallen. Bekämpft effizient Parasiten des Magen-Darm-Traktes, indem er fremde DNS angreift. Bereinigt den Darm und die Gallenabflusswege.",
+    
+    "lt61": "HANDROANTHUS",
+    
+    "lt62": "Lapachol ist eine besondere Verbindung, die in der Rinde enthalten ist, beugt die Vermehrung der pathogenen Keime vor, hemmt die Vermehrung und Entwicklung der zellinneren Parasiten.",
+    
+    "lt63": "SCHWARZNUSS",
+    
+    "lt64": "Elemente der Schwarznuss stören den Stoffwechsel der Würmer, lösen eine Lähmung der Muskelgewebe der Parasiten aus, sowie stärken die Abwehrkraft und die Schutzfunktionen des Organismus, reinigen das Blut von den Toxinen.",
+    
+    "lt65": "OLIVENBAUMBLÄTTER",
+    
+    "lt66": "Enthält Oleuropein, welches über eine parasitentötende und pilztötende Wirkung verfügt, vermindert die oxidative Schädigung im Blut, der Leber, den Nieren und den Hirngeweben.",
+    
+    "lt67": "KURKUMAWURZEL",
+    
+    "lt68": "In der Wurzel der Kurkuma ist Cineol, Kurkumin und Vitamin P enthalten. Cineol normalisiert die Funktion der Verdauungsorgane und dämmt die Lebenstätigkeit der Würmer ein, Kurkumin verfügt über eine parasitentötende Wirkung gegen Nematoden, Vitamin P stellt die Funktion der Organe der Hämopoese wieder her und bereinigt das Blut.",
+    
+    "lt69": "ASA FOETIDE",
+    
+    "lt70": "Ist bekannt dank ihren stärksten bakterientötenden und keimtötenden Eigenschaften. Insbesondere wird sie geschätzt dank ihrer Fähigkeit, die Parasiten Bandwürmer aus dem Magendarmtrakt herauszuführen.",
+    
+    "lt71": "WAS <span class=\"fadeIn wow\" style=\"visibility: hidden; animation-name: none;\">Parazitol</span>MACHT ES BESSER ALS ANDERE MITTEL?",
+    
+    "lt72": "CHEMISCHE MITTEL",
+    
+    "lt74": "CHEMISCHE MITTEL",
+    
+    "lt75": "Gefährliche chemische <br>Stoffe in der Rezeptur <i class=\"wow pulse minus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+    
+    "lt76": "Ein Schlag auf die inneren<br> Organe und Systeme <i class=\"wow pulse minus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+    
+    "lt77": "Trägt<br>zur Abschwächung der Abwehrkraft bei <i class=\"wow pulse minus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+    
+    "lt78": "Fügt Schaden den Organen des Magendarmtrakts:<br> <span>der Leber, des Magens, der Pankreas, dem Darm, provoziert<br> Gastritis und Geschwüre.</span> ",
+    
+    "lt80": "Eine vollkommen<br>natürliche Zusammensetzung <i class=\"wow pulse plus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+    
+    "lt81": "Verfügt über keine<br>Nebenwirkungen, schützt<br>innere Organe <i class=\"wow pulse plus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+    
+    "lt82": "Stärkt die Abwehrkraft <br>und Widerstandsfähigkeit<br> des Organismus gegen Infekte <i class=\"wow pulse plus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+    
+    "lt83": "Wirkt sich wohltuend auf den Magendarmtrakt aus:<br> <span>, verbessert den Gallenabfluss, stimuliert die Arbeit des Darms</span> ",
+    
+    "lt84": " <span>Parazitol</span> SANFT FÜR DEN MENSCHEN — HART FÜR DIE HELMINTHEN",
+    
+    "lt85": "„Die meisten Menschen ahnen es nicht einmal, dass der Organismus eines jeden zweiten mit PARASITEN BEFALLEN ist.",
+    
+    "lt86": "In 95% der Fälle löst die Präsenz der Helminthen keinerlei Symptome aus, bis zu dem Zeitpunkt, an dem sie irreparable Schaden den inneren Organen zugefügt haben und die Funktion aller Systeme des Organismus gestört haben.",
+    
+    "lt87": "Gegenwärtig ist nur das Mittel Parazitol in der Lage, den Organismus VOLLKOMEN von beliebigen Parasiten zu befreien, von denen es über 250 Arten gibt. Es erzeugt im menschlichen Organismus ein Mileau, das den Vorgang des Parasitierens an sich zerstört.",
+    
+    "lt88": "Die Erfahrung meiner Patienten bestätigt, dass Parazitol das wirksamste und sicherste Mittel für die Vernichtung und Ausleitung ALLER arten der Invasionen ist, einschließlich von Eiern, das keine Wiederansteckung zulässt.",
+    
+    "lt89": "Die positive Wirkung wird in 100% der Fälle erreicht“.",
+    
+    "lt90": "PD Dr. med. Wolfgang Schmidt, Infektologe",
+    
+    "lt91": "ZUSÄTZLICHE WIRKUNG <span class=\"fadeIn wow\" style=\"visibility: hidden; animation-name: none;\">Parazitol</span> ",
+    
+    "lt92": "Hilft bei der Reinigung der Organe und Gewebe, neutralisiert die Vergiftung infolge der parasitären Infektion",
+    
+    "lt93": "Regeneriert Schleimhäute, bereinigt das Blut, die Lymphe, die interzellulären Flüssigkeiten",
+    
+    "lt94": "Normalisiert den oxidativ-redoxidative Gleichgewicht, das mit der Bildung der Krebszellen droht",
+    
+    "lt95": "Normalisiert den oxidativ-redoxidative Gleichgewicht, das mit der Bildung der Krebszellen droht",
+    
+    "lt96": "Trägt zur Befreiung vom Geschwür und von Gastritis, indem es Bakterien tötet, die sie provozieren",
+    
+    "lt97": "Stellt die Funktionen der unterdrückten Organe wieder her und normalisiert sie",
+    
+    "lt98": "Wirkt wohltuend auf Organe des Magendarmtraktes",
+    
+    "lt99": "Verbessert den Zustand der Haut, der Haare und der Fingernägel",
+    
+    "lt100": "BESTELLEN",
+    
+    "lt101": "ERFAHRUNGEN DER KÄUFER",
+    
+    "lt102": "<div class=\"slide-description-title\">Uta Krueger, Stuttgart</div> „Mir hat dieses Mittel sehr gefallen. Ich und mein Ehemann haben es zu zweit eingenommen. Und beide hatten wir verschiedene Erscheinungen: wir bekamen kalten Schweiß, Durchfall, es entstanden einige Pickel und wir hatten üble Kopfschmerzen. Nach bereits 1,5 Wochen ist die akute Phase vergangen, und nun fühlen wir uns beide ausgezeichnet! Parazitol wirkt sehr sanft, jedoch stark und sicher“.",
+    
+    "lt103": "<div class=\"slide-description-title\">Dana, Ruhrgebiet</div> „Ich habe mich einige Wochen lang schlecht gefühlt, habe Tests gemacht und es stellte sich heraus, dass es Parasiten waren. Ich habe nach ärztlicher Verschreibung Tabletten eingenommen, es wurde aber jeden Tag nur noch schlimmer. Ich weiß nicht, was mit mir geworden wäre, wenn ich nicht von dem neuen Mittel Parazitol erfahren hätte. Eben das ist es, was mich gerettet hat. Einen Monat später habe ich wieder Tests gemacht, alles sauber. Nun werde ich Parazitol zweimal jährlich zur Prophylaxe trinken“.",
+    
+    "lt104": "<div class=\"slide-description-title\">Martin, Bremen</div>„Unser Kater kann nicht zu Hause sitzen, er wandert gern durch den Garten. So hat er uns dann auch Würmer gebracht. Mit mir war alles normal, die Frau hatte aber sehr gelitten. Erst hat sie nachts mit den Zähnen geknirscht, dann kamen auch Fieber und Erbrechen. Wir haben uns zusammen behandeln lassen, weil sie sehr wahrscheinlich auch mich anstecken konnte. Wir haben eine Kur von Parazitol durchlaufen und alles ist endlich vorbei“.",
+    
+    "lt105": "<div class=\"slide-description-title\">Uta Krueger, Stuttgart</div> „Mir hat dieses Mittel sehr gefallen. Ich und mein Ehemann haben es zu zweit eingenommen. Und beide hatten wir verschiedene Erscheinungen: wir bekamen kalten Schweiß, Durchfall, es entstanden einige Pickel und wir hatten üble Kopfschmerzen. Nach bereits 1,5 Wochen ist die akute Phase vergangen, und nun fühlen wir uns beide ausgezeichnet! Parazitol wirkt sehr sanft, jedoch stark und sicher“.",
+    
+    "lt106": "<div class=\"slide-description-title\">Dana, Ruhrgebiet</div> „Ich habe mich einige Wochen lang schlecht gefühlt, habe Tests gemacht und es stellte sich heraus, dass es Parasiten waren. Ich habe nach ärztlicher Verschreibung Tabletten eingenommen, es wurde aber jeden Tag nur noch schlimmer. Ich weiß nicht, was mit mir geworden wäre, wenn ich nicht von dem neuen Mittel Parazitol erfahren hätte. Eben das ist es, was mich gerettet hat. Einen Monat später habe ich wieder Tests gemacht, alles sauber. Nun werde ich Parazitol zweimal jährlich zur Prophylaxe trinken“.",
+    
+    "lt107": "<div class=\"slide-description-title\">Martin, Bremen</div> „Unser Kater kann nicht zu Hause sitzen, er wandert gern durch den Garten. So hat er uns dann auch Würmer gebracht. Mit mir war alles normal, die Frau hatte aber sehr gelitten. Erst hat sie nachts mit den Zähnen geknirscht, dann kamen auch Fieber und Erbrechen. Wir haben uns zusammen behandeln lassen, weil sie sehr wahrscheinlich auch mich anstecken konnte. Wir haben eine Kur von Parazitol durchlaufen und alles ist endlich vorbei“.",
+    
+    "lt108": "<div class=\"slide-description-title\">Uta Krueger, Stuttgart</div> „Mir hat dieses Mittel sehr gefallen. Ich und mein Ehemann haben es zu zweit eingenommen. Und beide hatten wir verschiedene Erscheinungen: wir bekamen kalten Schweiß, Durchfall, es entstanden einige Pickel und wir hatten üble Kopfschmerzen. Nach bereits 1,5 Wochen ist die akute Phase vergangen, und nun fühlen wir uns beide ausgezeichnet! Parazitol wirkt sehr sanft, jedoch stark und sicher“.",
+    
+    "lt110": "EIN HARTER SCHLAG GEGEN DIE PARASITEN",
+    
+    "lt111": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Normalisiert die Funktion des Magendarmtraktes",
+    
+    "lt112": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Vernichtet sämtliche Arten von Helminthen",
+    
+    "lt113": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Leitet die Produkte der Lebenstätigkeit der Parasiten heraus",
+    
+    "lt114": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Beugt einen Wiederbefall vor",
+    
+    "lt115": "EIN SONDERANGEBOT<br>",
+    
+    "lt116": "bis zum Ende der Aktion verbleiben",
+    
+    "lt117": "<div class=\"price_old\"></div>  Normalpreis",
+    
+    "lt118": "<div class=\"price_main wow pulse\" data-wow-delay=\"2s\" data-wow-iteration=\"3\" style=\"visibility: hidden; animation-delay: 2s; animation-iteration-count: 3; animation-name: none;\"></div>  Aktionspreis",
+    
+    "lt119": "Bestellen",
+    
+    "lt120": "Wie wir arbeiten",
+    
+    "lt121": "Hinterlassen Sie eine Anfrage auf unserer Webseite",
+    
+    "lt122": "Die Fachkraft wird Kontakt mit Ihnen zur Bestätigung der Bestellung aufnehmen",
+    
+    "lt123": "Bezahlung bei dem Empfang der Bestellung"
+    
+
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
